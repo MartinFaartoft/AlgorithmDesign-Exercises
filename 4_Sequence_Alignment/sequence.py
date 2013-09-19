cdace2d11265abc0384e3aa6a5ae0893e17cddf4
Martin Faartoft <martin.faartoft@gmail.com>
2013-09-19 14:42:54 +0200

    lol

--- 4_Sequence_Alignment/sequence.py
+++ 4_Sequence_Alignment/sequence.py
@@ -2,7 +2,25 @@ import sys
 
 def sequence_alignment():
 	#TODO: implement alignment algorithm
-	pass:
+	pass
+
+def parse_blosum():
+	filename = '../data/BLOSUM62.txt'
+	with open(filename, 'r') as f: 
+		data = f.read().splitlines()[6:]
+	chars = data[0].split()
+	
+	blosum = {char:{} for char in chars}
+
+	for line in data[1:]:
+		sub_split = line.split()
+		from_char = sub_split[0]
+		for index, value in enumerate(sub_split[1:]):
+			to_char = chars[index]
+			blosum[from_char][to_char] = int(value)
+
+	return blosum
+
 
 def parse_data():
 	data = sys.stdin.read().splitlines()
@@ -21,6 +39,7 @@ def parse_data():
 	seq_data.append((name,seq))
 	return seq_data
 
+parse_blosum()
 seq_data = parse_data()
 # for Toy_FASTA.in:
 # seq_data = [('Sphinx', 'KQRK'), ('Bandersnatch', 'KAK'), ('Snark', 'KQRIKAAKABK')]
